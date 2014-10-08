9fea0cefd9cb75508a8ceac9e92da4b6f2ebb4bb
trojanspike_ <trojan_spikepk@yahoo.co.uk>
2014-10-08 15:15:11 +0100

    updates with bower & filter library

--- app/scripts/constants/settings.js
+++ app/scripts/constants/settings.js
@@ -3,8 +3,15 @@
 window.angular.module('app.constants',[])
 .constant('apiInfo', {
 /* url-main, markers, details */
-		url : 'http://aa-finder-data.herokuapp.com/data.json'
+		url : 'https://aa-finder-data.herokuapp.com/data.json'
 
 })
-.constant('devinfo', {})
-.constant('appinfo', {});
+.constant('devinfo', {
+        name : 'Lee Mc Kay',
+        url : 'sites-ignite.co.uk',
+        email : 'aafinder@sites-ignite.co.uk'
+    })
+.constant('appinfo', {
+        version : '',
+        buildDate : ''
+    });
