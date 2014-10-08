65d0bbbc77cd1e662aa4d93615e03655be7cd67c
trojanspike_ <trojan_spikepk@yahoo.co.uk>
2014-10-08 15:14:20 +0100

    setup data into views

---
+++ app/scripts/controllers/domControllers.js
@@ -0,0 +1,14 @@
+'use strict';
+
+window.angular.module('app.domControllers', [])
+.controller('menuCtrl', ['$scope', 'towns', function($scope, towns){
+        window.modal.show();
+        towns.then(function(data){
+            $scope.towns = data;
+            $scope.$apply();
+            window.modal.hide();
+        });
+}])
+.controller('activeController', ['$route', '$scope', function($route, $scope){
+    $scope.$route = $route;
+}]);
--- app/scripts/factories/appData.js
+++ app/scripts/factories/appData.js
@@ -1,34 +1,52 @@
 'use strict';
 window.angular.module('app.data', ['app.constants'])
-    .service('appService', ['$http','$q', 'apiInfo',function($http, $q, apiInfo){
-        var _Cache, _Crypt, _Data;
-        /* Construct ran in #/welcome */
-        this.ready = function(callback){
-            (function CheckerCache(){
-                if( typeof _Cache === 'undefined' ) {
-                    setTimeout(CheckerCache, 500);
+.service('appService', ['$http','$q', 'apiInfo',function($http, $q, apiInfo){
+    var _Cache, _Crypt, _Meetings, _Settings, self = this;
+
+    this.Settings = _Settings;
+    this.Meetings = _Meetings;
+    /* Construct ran in #/welcome */
+    this.ready = function(callback){
+        (function CheckerCache(){
+            if( typeof _Cache === 'undefined' ) {
+                setTimeout(CheckerCache, 500);
+            } else {
+                /* Lets set up settings  */
+                self.Settings = _Cache.container('settings');
+                /* Lets check for meetings */
+                if( _Cache.list().indexOf('meetings') < 0 ){
+                    self.Meetings = _Cache.container('meetings');
+                    $http({
+                        method : 'GET',
+                        url : apiInfo.url
+                    }).then(function(obj){
+                        self.Meetings.put( JSON.stringify(obj.data) ).save(callback);
+                    });
                 } else {
-                    /* Lets check for data */
-                    if( _Cache.list().indexOf('data') < 0 ){
-                        _Data = _Cache.container('data');
-                        $http({
-                            method : 'GET',
-                            url : apiInfo.url
-                        }).then(function(obj){
-                            _Data.put( JSON.stringify(obj.data) ).save();
-                            callback();
-                        });
-                    } else {
-                        _Data = _Cache.container('data');
-                        callback();
-                    }
+                    self.Meetings = _Cache.container('meetings');
+                    callback();
                 }
-            })();
-        };
-        this.setCache = function(c){
-            _Cache = c;
-        };
-        this.setCrypt = function(c){
-            _Crypt = c;
-        };
-    }]);
+            }
+        })();
+    };
+    this.setCache = function(c){
+        _Cache = c;
+    };
+    this.setCrypt = function(c){
+        _Crypt = c;
+    };
+}])
+.factory('towns', ['appService', '$q', function(appService, $q){
+    /* return available towns */
+        var defer = $q.defer();
+        appService.ready(function(){
+            var towns = [], Data = JSON.parse( appService.Meetings.get() );
+            for( var t in Data ){
+                if( towns.indexOf(Data[t].town) < 0 ){
+                    towns.push( Data[t].town );
+                }
+            }
+            defer.resolve( towns );
+        });
+        return defer.promise;
+}]);
