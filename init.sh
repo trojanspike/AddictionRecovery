3ad63be885de36bf626ed228d36b8a998d55b689
lee <trojanspike>
2014-10-27 13:38:35 +0000

    bower and index

--- init.sh
+++ init.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
+mkdir www && cordova platform add android &&
+npm i && bower i && cd ./test/ && bower i && cd ../ && grunt build
 
 for i in $(cat ./Plugins.txt | sed 's/#.*//'); 
 do 
 	cordova plugin add $i; 
 done
-
-cordova platform add android
-
-npm i && bower i && cd ./test/ && bower i && cd ../ && grunt build && grunt serve
